4f451a0035573dcc51db797685dda3839c8cf81b
Kevin Cabral <kevin1tdsb@gmail.com>
2023-02-05 00:01:17 -0300

    primeiro commit

---
+++ exercicio003/main.js
@@ -0,0 +1,41 @@
+const form = document.getElementById('form-validar')
+const result = document.getElementById('result')
+
+formEValido = false
+function validarNumero(numberA, numberB){
+    if(numberB>numberA){
+        return formEValido = true
+    }else{
+        return formEValido = false
+    }
+}
+
+
+function sucesso(){
+    result.style.display = 'block'
+    result.style.padding = "5px 20px"
+    result.style.backgroundColor = "rgb(30, 255, 0)"
+    result.innerHTML = "Sucesso"
+}
+function erro(){
+    result.style.display = 'block'
+    result.style.padding = "5px 20px"
+    result.style.backgroundColor = "rgb(231, 12, 12)"
+    result.innerHTML = "Erro"
+}
+
+
+form.addEventListener('submit', function(e) {
+    e.preventDefault()
+
+    var numberA = document.getElementById('numeroA').value
+    var numberB = document.getElementById('numeroB').value
+
+    formEValido = validarNumero(numberA, numberB)
+
+    if (formEValido === true){
+        sucesso()
+    }else{
+        erro()
+    }
+})
