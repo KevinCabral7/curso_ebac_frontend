dc5e502f62ec720a1b30dda66b07b159fbedf6d5
Kevin Cabral <83372907+KevinCabral7@users.noreply.github.com>
2023-07-26 13:47:57 -0300

    exercicio ts

---
+++ exercicio.js
@@ -0,0 +1,5 @@
+"use strict";
+const multiplicacao = (num1, num2) => num1 * num2;
+const saudacao = (nome) => `Olá ${nome}`;
+console.log(multiplicacao(5, 3));
+console.log(saudacao("Kevin"));
---
+++ exercicio.ts
@@ -0,0 +1,7 @@
+const multiplicacao = (num1: number, num2: number): number => num1 * num2
+
+const saudacao = (nome: string): string => `Olá ${nome}`
+
+
+console.log(multiplicacao(5, 3))
+console.log(saudacao("Kevin"))
