c64d9ae53b49a0e995c789b103d286dd71b703c1
Kevin Cabral <83372907+KevinCabral7@users.noreply.github.com>
2023-08-02 14:29:51 -0300

    exercicio vue

---
+++ src/App.vue
@@ -0,0 +1,41 @@
+<script setup>
+import { reactive } from 'vue';
+const estado = reactive({
+  value1: 0,
+  value2: 0,
+  operacao: 'adicao',
+  resultado: 0,
+});
+
+function calcula() {
+  const { value1, value2, operacao } = estado;
+  switch (operacao) {
+    case '+':
+      estado.resultado = value1 + value2;
+      break;
+    case '-':
+      estado.resultado = value1 - value2;
+      break;
+    case '*':
+      estado.resultado = value1 * value2;
+      break;
+    case '/':
+      estado.resultado = value1 / value2;
+      break;
+    default:
+      estado.resultado = 0;
+  }
+}
+</script>
+
+<template>
+  <input type="number" v-model="estado.value1" @keyup="calcula" />
+  <select v-model="estado.operacao" @change="calcula">
+    <option value="+">+</option>
+    <option value="-">-</option>
+    <option value="*">*</option>
+    <option value="/">/</option>
+  </select>
+  <input type="number" v-model="estado.value2" @keyup="calcula" />
+  <p>{{ estado.resultado }}</p>
+</template>
---
+++ src/components/Calculadora.vue
@@ -0,0 +1,41 @@
+<template>
+    <div>
+        <input type="number" v-model="value1" @keyup="calcula">
+        <select v-model="operacao" @change="calcula">
+            <option value="adicao">+</option>
+            <option value="subtracao">-</option>
+            <option value="multiplicacao">*</option>
+            <option value="divisao">/</option>
+        </select>
+        <input type="number" v-model="value2" @keyup="calcula">
+        <p>{{ resultado }}</p>
+    </div>
+</template>
+  
+<script setup>
+import { ref } from 'vue';
+const value1 = ref(0);
+const value2 = ref(0);
+const operacao = ref('adicao');
+const resultado = ref(0);
+
+function calcula() {
+    switch (operacao.value) {
+        case 'adicao':
+            resultado.value = value1.value + value2.value;
+            break;
+        case 'subtracao':
+            resultado.value = value1.value - value2.value;
+            break;
+        case 'multiplicacao':
+            resultado.value = value1.value * value2.value;
+            break;
+        case 'divisao':
+            resultado.value = value1.value / value2.value;
+            break;
+        default:
+            resultado.value = 0;
+    }
+}
+</script>
+  
